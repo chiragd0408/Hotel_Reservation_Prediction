49e111ed0499132eab4ddebffc207b2254fa6b7a
chiragd0408 <chiragranpise48@gmail.com>
2025-06-10 18:34:35 +0530

    ...

---
+++ src/logger.py
@@ -0,0 +1,9 @@
+import logging
+import os
+from datetime import datetime
+
+LOGS_DIR = "logs",
+os.makedirs(LOGS_DIR,exist_ok=True)
+
+LOG_FILE = os.path.join(LOGS_DIR, f"log_{datetime.now().strftime('%Y-%m-%d')}.log")
+
