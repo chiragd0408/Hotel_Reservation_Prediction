eb10789d9cd90ed033943e6deb775fd99bedd7d5
chiragd0408 <chiragranpise48@gmail.com>
2025-08-21 11:30:56 +0530

    changes

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,7 +1,6 @@
 import os
 import pandas as pd
 import numpy as np
-from src.logger import get_logger
 from src.custom_exception import CustomException
 from config.paths_config import * 
 from utils.common_functions import read_yaml,load_data
